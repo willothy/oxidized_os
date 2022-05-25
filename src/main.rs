84ff364a39d52aadf2331343609f455b87137441
Will Hopkins <willothyh@gmail.com>
2022-05-24 20:42:16 -0700

    VGA text mode

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::fmt::Write;
+
+mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     // Replaces panic since stdlibs aren't accessible
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // Entry point replacing main
+    println!("Welcome!");
+    println!("This is Oxidized OS.");
     loop {}
 }
